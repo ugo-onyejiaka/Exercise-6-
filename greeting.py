0303479b45a41a597b9ec7554044aded799e35f2
Ugo Onyejiaka <ugoonyejiaka4@gmail.com>
2024-10-15 17:12:44 -0400

    Added say_goodbye function

---
+++ greeting.py
@@ -0,0 +1,27 @@
+"""
+This script contains functions for basic greetings.
+
+Functions:
+    say_hello: Prints a greeting message.
+"""
+
+def say_hello(name):
+    """Prints a greeting message.
+
+    Args:
+        name (str): The name of the person to greet.
+
+    Returns:
+        None
+    """
+    print(f"Hello, {name}!")
+def say_goodbye(name):
+    """Prints a farewell message.
+
+    Args:
+        name (str): The name of the person to say goodbye to.
+
+    Returns:
+        None
+    """
+    print(f"Goodbye, {name}!")
